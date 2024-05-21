efc8dfb7c3f89e8f06400d0e6744436b43ea8461
ResinDh <ferizi10rasim@gmail.com>
2024-05-21 21:22:36 +0200

    v0
    Combined Scenes

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,28 @@ using UnityEngine.Events;
 
 public class PlayerInput : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnTurretMove = new UnityEvent<Vector2>();
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnShoot = new UnityEvent();
 
     void Update()
     {
         GetMovement();
+        GetTurretMovement();
+        GetShootingInput();
+    }
+
+    private void GetShootingInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnShoot?.Invoke();
+        }
+    }
+
+    private void GetTurretMovement()
+    {
+        
     }
 
     private void GetMovement()
